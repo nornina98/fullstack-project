53cb0bb10ef42b65e29cd2e87ae42607232c86c8
Nor Ikram <nornina8998@gmail.com>
2024-03-12 12:35:41 +0800

    work on planets routes and fix cors policy origin

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const planetsRouter = require("./routes/planets/planets.router");
+
+// Define routes or middleware using express json
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = app;
