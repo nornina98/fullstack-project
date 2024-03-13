1a9b161b66c0adc9141c390f6db33f9e2eabb2dd
Nor Ikram <nornina8998@gmail.com>
2024-03-13 15:55:49 +0800

    config scripts on package.json to run simultaneous between server and client on different port

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+
+const path = require("path");
+
 const app = express();
 
 const planetsRouter = require("./routes/planets/planets.router");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Define routes or middleware using express json
 app.use(
