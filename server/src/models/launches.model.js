7f1a5eb78999872903eb3c948b04e4221cd2bdcd
Ikram <nornina8998@gmail.com>
2024-03-24 08:44:37 +0800

    connected dashboard for set launch into back-end

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,11 +3,11 @@ const launches = new Map();
 let latestFlightNumber = 100;
 
 const launch = {
-  flightNumber: 1,
+  flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-422 b",
+  target: "Kepler-422 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
