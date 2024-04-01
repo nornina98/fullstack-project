0e3064e6af80f0aa831661a5d507e856c5e78ab9
IKRAM-LAPTOP <nornina8998@gmail.com>
2024-04-02 07:26:15 +0800

    fix git conflict merge

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -30,12 +35,13 @@ function httpAddNewLaunch(req, res) {
 }
 
 function httpAbortLaunch(req, res) {
-  const launchId = req.params.id;
-  // if not found or exist
-  return res.status(404).json({
-    error: "Launch not found",
-  });
-
+  const launchId = Number(req.params.id);
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+  const aborted = abortLaunchById(launchId);
   return res.status(200).json(aborted);
 }
 
